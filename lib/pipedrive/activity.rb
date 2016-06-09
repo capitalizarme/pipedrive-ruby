4c9c43f6b0163a903fac38321f8e3d9e7c02568e
Jean Droguett <jdroguett@gmail.com>
2016-06-09 13:09:57 -0400

    delete activity

--- lib/pipedrive/activity.rb
+++ lib/pipedrive/activity.rb
@@ -1,4 +1,9 @@
 module Pipedrive
   class Activity < Base
+    def delete
+      opts = {id: self.id}
+      res = delete "#{resource_path}/#{id}", :body => opts
+      res.success? ? res['data']['id'] : bad_response(res,opts)
+    end
   end
 end
